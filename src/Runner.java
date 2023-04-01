79390c2e3390fb6c9b3ce29ac7bbea7b132ac60b
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-04-01 20:39:09 +0200

    add Runner methods

--- src/Runner.java
+++ src/Runner.java
@@ -1,117 +1,78 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 public class Runner {
-  public static final int MIN_X = -15;
-  public static final int MAX_X = 15;
-  public static final int MIN_Y = -15;
-  public static final int MAX_Y = 15;
-  public static int fieldX;
-  public static int fieldY;
-  public static int center;
+  public static final String CYAN = "\u001B[36m";
+  public static final String RESET = "\u001B[0m";
+  public static final String GREEN = "\u001B[32m";
 
-  public static void main(String[] args) throws IOException {
+
+  public static void main(String[] args) throws IOException, InterruptedException, AWTException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    fieldX = MAX_X - MIN_X;
-    fieldY = MAX_Y - MIN_Y;
-    int radius = 10;
-    char[][] charArray = new char[fieldY][fieldX];
-    buildField(charArray);
-    buildCircle(charArray, radius);
-    placePoint(charArray, Point.read(br), radius);
-    showField(charArray);
+    Field field = new Field();
+    messagePrint(CYAN + "Укажите координаты Вашей точки через запятую\n");
+    messagePrint("Значение X и Y должны находиться в пределах от "
+                 + (Field.MIN_X + 1) + " до " + (Field.MAX_X - 1));
+    messagePrint(RESET + "\nВведите координаты [x,y]: ");
+    Point point = Point.parse(br);
+    System.out.println();
+    messagePrint(CYAN + "Укажите радиус окружности с центром в точке O (0, 0)\n");
+    messagePrint("Значение должно находиться в пределах от 1 до " + (Field.center - 1));
+    messagePrint(RESET + "\nВведите радиус R: ");
+    int radius = readIntLimited(1, Field.center - 1);
+    System.out.println();
+    System.out.print(CYAN + "\nПроводим необходимые вычисления" + RESET + " [");
+    field.buildField();
+    field.buildCircle(radius);
+    field.placePoint(point, radius);
+    System.out.println("]");
+    System.out.println();
+    System.out.println(GREEN + "... нажмите ENTER для просмотра результата ..." + RESET);
+    br.readLine();
+    clearScreen();
+    System.out.println(CYAN+field.checkPointQuarter(point));
+    System.out.println(field.checkPointCircle(radius, point)+RESET);
+    field.showField();
   }
 
-  public static String checkPoint(int x, int y, int radius,Point point) {
-    String result1 = "";
-    String result2 = "";
-    if (x == center && y == center) {
-      result1 = "попали в центр";
-    } else if (x == center) {
-      result1 = "попали в Y";
-    } else if (y == center) {
-      result1 = "попали в X";
-    }
-    if (x > center && y > center) {
-      result1 = "находимся в IV координатной четверти";
-    }
-    if (x < center && y > center) {
-      result1 = "находимся в III координатной четверти";
-    }
-    if (x < center && y < center) {
-      result1 = "находимся в II координатной четверти";
-    }
-    if (x > center && y < center) {
-      result1 = "находимся в I координатной четверти";
-    }
-    x=point.getX();
-    y=point.getY();
-    int testPoint = (int) Math.round((Math.sqrt((x * x) + (y * y))));
-    if (testPoint < radius) {
-      result2 = "находимся внутри круга";
-    } else if (testPoint > radius) {
-      result2 = "находимся снаружи круга";
-    } else {
-      result2 = "находимся на границе круга";
-    }
-    return result1 + "," + result2;
-  }
-
-  public static void placePoint(char[][] charArray, Point point, int radius) {
-    int x = center + point.getX();
-    int y = center - point.getY();
-    charArray[y][x] = '◉';
-    System.out.println(checkPoint(x, y, radius,point));
-  }
-
-  public static void buildCircle(char[][] charArray, int radius) {
-    for (int row = 0; row < fieldY; row++) {
-      for (int column = 0; column < fieldX; column++) {
-        int x = column + MIN_X;
-        int y = row + MIN_Y;
-        int point = (int) Math.round((Math.sqrt((x * x) + (y * y))));
-        if (point == radius) {
-          charArray[row][column] = '.';
-        }
+  public static int readIntLimited(int min, int max) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = 0;
+    do {
+      try {
+        num = Integer.parseInt(br.readLine());
+      } catch (NumberFormatException e) {
+        System.out.println("Вводите только цифры!");
       }
-    }
-
-  }
-
-  public static void buildField(char[][] charArray) {
-    center = fieldX / 2;
-    for (int row = 0; row < fieldY; row++) {
-      for (int column = 0; column < fieldX; column++) {
-        charArray[row][column] = ' ';
-        if (column == center) {
-          charArray[row][column] = '│';
-        }
-        if (row == center) {
-          charArray[row][column] = '─';
-        }
-        if (column == center && row == center) {
-          charArray[row][column] = '┿';
-        }
+      if (!(num >= min && num <= max)) {
+        System.out.printf("Введите число от %d до %d: ", min, max);
       }
-    }
-    charArray[0][center] = '△';
-    charArray[0][center + 1] = 'Y';
-    charArray[center][fieldX - 1] = '▷';
-    charArray[center - 1][fieldX - 1] = 'X';
-    charArray[center - 1][center + 1] = '0';
+    } while (!(num >= min && num <= max));
+    return num;
   }
 
-  public static void showField(char[][] charArray) {
-    for (int i = 0; i < fieldY; i++) {
-      for (int j = 0; j < fieldX; j++) {
-        if (i == center) {
-          System.out.print("──" + charArray[i][j]);
-        } else {
-          System.out.print("  " + charArray[i][j]);
-        }
-      }
-      System.out.println();
+  public static void clearScreen() throws AWTException {
+    Robot robot = new Robot();
+    robot.keyPress(KeyEvent.VK_CONTROL);
+    robot.keyPress(KeyEvent.VK_ALT);
+    robot.keyPress(KeyEvent.VK_SHIFT);
+    robot.keyPress(KeyEvent.VK_Q);
+    robot.keyRelease(KeyEvent.VK_Q);
+    robot.keyRelease(KeyEvent.VK_SHIFT);
+    robot.keyRelease(KeyEvent.VK_ALT);
+    robot.keyRelease(KeyEvent.VK_CONTROL);
+  }
+
+  public static void messagePrint(String massage) throws InterruptedException {
+    for (int i = 0; i < massage.length(); i++) {
+      System.out.print(massage.charAt(i));
+      TimeUnit.MILLISECONDS.sleep(5);
     }
   }
+
+
 }
